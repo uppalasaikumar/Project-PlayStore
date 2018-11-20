688ccd0edcd5bee57b60dc68c1912659876b695b
paul-gomes <paulgomez1994@gmail.com>
2018-11-19 19:53:26 -0600

    Mapper and reducer completed

--- priceVsInstalls/mapper.py
+++ priceVsInstalls/mapper.py
@@ -1,16 +1,18 @@
+
 import csv
 
-c = open("mapperOutput.txt", "w")
+c = open("./output/mapperOutput.txt", "w")                                       # opening the file to write
 
-with open("googleplaystore.csv", "rb") as csvFile:
-    reader = csv.reader(csvFile, delimiter=',')
-    line_count = 0;
-    for row in reader:
-        if (line_count != 0):
-            if (len(row) == 13):
-                price = row[7].replace("$", "")
-                installs = row[5].replace("+", "").replace(",", "")
-                c.write(price + "," + installs + "\n")
+with open("../Data/googleplaystore.csv", "rb") as csvFile:                       #Reading CSV file from the specific loaction
+    reader = csv.reader(csvFile, delimiter=',')                                  #Reader reads the whole file
+    line_count = 0;                                                              #Keeping track of line count to skip the first line
+    for row in reader:                                                           #Goes through each row in reader
+        if (line_count != 0):                                                    #Skips the first line
+            if (len(row) == 13):                                                 #Only proceeds if the specific row has 13 columns
+                price = row[7].replace("$", "")                                  #Gets rid of $ sign from the price
+                installs = row[5].replace("+", "").replace(",", "")              #Gets rid of + and , symbol from the installs
+                c.write(price + "\t" + installs + "\n")                          #writes the price and installs as a tab separated values
         line_count += 1
 
 c.close()
+#
