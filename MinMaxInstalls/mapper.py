798f8a6729e5561f872d80834bfe4e298368832e
Sai kumar Uppala <saikumar.uppala217@gmail.com>
2018-11-29 14:20:56 -0600

    filtered data set

--- MinMaxInstalls/mapper.py
+++ MinMaxInstalls/mapper.py
@@ -6,11 +6,11 @@ for line in f:
     # stripping and splitting line by ","
     data = line.strip().split(",") 
     # checking if each line has length of 15
-    if len(data) == 15:
+    if len(data) == 13:
         # Assiging values to all these variables
-        App,Category,Rating,Reviews,Size,Installs,Install_tail,Type,Price,Content_Rating,Genres,Last_Updated,Last_Updated_tail,Current_Ver,Android_Ver = data
+        App,Category,Rating,Reviews,Size,Installs,Type,Price,Content_Rating,Genres,Last_Updated,Last_Updated_tail,Current_Ver,Android_Ver = data
         #slicing the strings
-        Total_Installs = Installs[1:] + ""+ Install_tail[:len(Install_tail)-2]
+        #Total_Installs = Installs[1:] + ""+ Install_tail[:len(Install_tail)-2]
         #writing the data to o.txt file and formatting mapperoutput
         o.write("{0}\t{1}\n".format(Category, Total_Installs)) 
 f.close()
