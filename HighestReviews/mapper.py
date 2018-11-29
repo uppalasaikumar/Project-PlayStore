9e687b7c317a2b5d04a6dbe25f4f97079df07b32
Vipul Chandoor <36544701+chandoorvipul@users.noreply.github.com>
2018-11-29 16:01:20 -0600

    Update mapper.py

--- HighestReviews/mapper.py
+++ HighestReviews/mapper.py
@@ -1,21 +1,12 @@
 f = open("../Data/googleplaystore.csv","r")  # open file, read-only
 o = open("./output/o.txt", "w") # open file, write
-# count = 0
-for line in f:
-    # count = count + 1
-    data = line.strip().split(",") 
-    # print(data)
-    # if count > 2:
-    #     breakcd
-    if len(data) == 13:
+for line in f: # iterate through all rows in a file
+    data = line.strip().split(",") # trimming the record to remove whitespaces
+    if len(data) == 13: # checking whether the length of the record is 13 or not to avoid bad data
         App, Category, Rating, Reviews, size, installs, Type, Price, contentRating, genres, lastUpdated, currentVer, AndroidVer = data
-        # print(int(Price) == 0)
-        if Price == '0':
-            if '"' in Category:
-                continue
-            # print(Category)
-            # print(App)
-            # print(Reviews)
-            o.write("{0}\t{1}\t{2}\n".format(Category, App, Reviews))
-f.close()
-o.close()
+        if Price == '0': # writing record to file only if price is 0
+            if '"' in Category: # Checking that category needs to have '"'
+                continue # continue to next iteration
+            o.write("{0}\t{1}\t{2}\n".format(Category, App, Reviews)) # writing record to a file
+f.close() # closing the input data file
+o.close() # closing the output file
