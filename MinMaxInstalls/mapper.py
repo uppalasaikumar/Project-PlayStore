6a6ec9a061e233bc89512685732f72c8241af0d1
Sai kumar Uppala <saikumar.uppala217@gmail.com>
2018-11-29 16:39:38 -0600

    modified comments in mapper

--- MinMaxInstalls/mapper.py
+++ MinMaxInstalls/mapper.py
@@ -5,13 +5,10 @@ linecount = 1
 for line in f:
     # stripping and splitting line by ","
     data = line.strip().split(",") 
-    # checking if each line has length of 15
+    # checking if each line has length of 13
     if len(data) == 13 and linecount != 1:
         # Assiging values to all these variables
-        #App,Category,Rating,Reviews,Size,Installs,Type,Price,Content_Rating,Genres,Last_Updated,Last_Updated_tail,Current_Ver,Android_Ver = data
         app, Category, rating, reviews, size, installs, Type, price, contentRating, genres, lastUpdated, currentVer, AndroidVer = data
-        #slicing the strings
-        #Total_Installs = Installs[1:] + ""+ Install_tail[:len(Install_tail)-2]
         #writing the data to o.txt file and formatting mapperoutput
         o.write("{0}\t{1}\n".format(Category, installs)) 
     linecount = linecount+1
